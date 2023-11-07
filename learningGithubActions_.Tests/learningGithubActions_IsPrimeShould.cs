6a0e6c15325e131c33321ce6d9355670e777d0de
Jørund Jenssen Berg <jjberg@gmail.com>
2023-11-07 08:49:08 +0100

    Initial commit

---
+++ learningGithubActions_.Tests/learningGithubActions_IsPrimeShould.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using learningGithubActions_.Services;
+
+namespace learningGithubActions_.UnitTests.Services
+{
+    public class learningGithubActions__IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public learningGithubActions__IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
